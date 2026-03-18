d0c107c9eddbaea6fac33deb5022f48d74dc5f68
Magno-Rodrigues <cmrda@mail.com>
2026-03-18 11:53:45 -0300

    Add migration to merge heads b953 and i1j2

---
+++ migrations/versions/c7a9d2e4f1b0_merge_heads_b953_and_i1j2.py
@@ -0,0 +1,22 @@
+"""Merge heads b9538565cb22 and i1j2k3l4m5n6
+
+Revision ID: c7a9d2e4f1b0
+Revises: b9538565cb22, i1j2k3l4m5n6
+Create Date: 2026-03-18
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "c7a9d2e4f1b0"
+down_revision = ("b9538565cb22", "i1j2k3l4m5n6")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
+
